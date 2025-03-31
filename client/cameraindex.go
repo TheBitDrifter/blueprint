2dc67567fa5bf32833141f0276cb2a8414c71aeb
TheBitDrifter <TheBitDrifter@gmail.com>
2025-03-31 00:47:50 -0400

    Clearing files to avoid confusing namespace

--- client/cameraindex.go
+++
@@ -1,5 +0,0 @@
-package client
-
-// The index/location of a coldbrew.Camera
-// Used to map or associate an entity to a Camera
-type CameraIndex int
--- client/constants.go
+++
@@ -1,8 +0,0 @@
-package client
-
-const (
-	SPRITE_LIMIT = 40 // Max sprites per bundle
-	ANIM_LIMIT   = 40 // Max animations per sprite
-	SOUND_LIMIT  = 40 // Max sounds per bundle
-	MaxSplit     = 8  // Max number of players/cameras/receivers
-)
